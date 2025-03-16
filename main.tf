ac4708b1ea8cfad65a92d989cbca023e3236522c
AbdullahTarar <abdullahhaider222@gmail.com>
2025-03-17 04:41:13 +0500

    helm release for node app added

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 module "vpc" {
   source                 = "./aws-modules/vpc"
   availability_zones     = var.availability_zones
@@ -6,44 +5,41 @@ module "vpc" {
   public_subnet_cidrs    = var.public_subnet_cidrs
   private_subnet_cidrs   = var.private_subnet_cidrs
   project_name           = var.project_name
-  ssh-port = var.ssh-port
-  http-port = var.http-port
-  https-port = var.https-port
+  ssh-port               = var.ssh-port
+  http-port              = var.http-port
+  https-port             = var.https-port
 }
 
-  module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "19.20.0"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.20.0"
 
-    cluster_name                    = var.cluster_name
-    cluster_version                 = var.cluster_version
-    cluster_endpoint_private_access = true
-    cluster_endpoint_public_access  = true
-    cluster_additional_security_group_ids = [module.vpc.ec2_sg_id]
+  cluster_name                    = var.cluster_name
+  cluster_version                 = var.cluster_version
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+  cluster_additional_security_group_ids = [module.vpc.ec2_sg_id]
 
-    vpc_id     = module.vpc.vpc_id
-    subnet_ids = module.vpc.private_subnet_ids
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
 
-    eks_managed_node_group_defaults = {
-      ami_type               = "AL2_x86_64"
-      disk_size              = 20
-      instance_types         = ["t2.micro"]
-      vpc_security_group_ids = [module.vpc.ec2_sg_id]
-    }
-
-    eks_managed_node_groups = {
-      green = {
-        min_size     = 1
-        max_size     = 3
-        desired_size = 2
-
-        instance_types = ["t2.micro"]
-        capacity_type  = "SPOT"
-
-    }
-
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 20
+    instance_types         = ["t2.micro"]
+    vpc_security_group_ids = [module.vpc.ec2_sg_id]
   }
+
+  eks_managed_node_groups = {
+    green = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+      instance_types = ["t2.micro"]
+      capacity_type  = "SPOT"
+    }
   }
+}
 
 module "lb_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
@@ -54,7 +50,6 @@ module "lb_role" {
   oidc_providers = {
     main = {
       provider_arn = module.eks.oidc_provider
-    
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
   }
@@ -62,11 +57,11 @@ module "lb_role" {
 
 resource "kubernetes_service_account" "service-account" {
   metadata {
-    name = "aws-load-balancer-controller"
+    name      = "aws-load-balancer-controller"
     namespace = "kube-system"
     labels = {
-        "app.kubernetes.io/name"= "aws-load-balancer-controller"
-        "app.kubernetes.io/component"= "controller"
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
     }
     annotations = {
       "eks.amazonaws.com/role-arn" = module.lb_role.iam_role_arn
@@ -80,10 +75,9 @@ resource "helm_release" "lb" {
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
-  version = "1.4.1"
-  depends_on = [
-    kubernetes_service_account.service-account
-  ]
+  version    = "1.4.1"
+
+  depends_on = [kubernetes_service_account.service-account]
 
   set {
     name  = "clusterName"
@@ -114,8 +108,60 @@ resource "helm_release" "lb" {
     name  = "serviceAccount.name"
     value = "aws-load-balancer-controller"
   }
-
-
 }
 
 
+### **Helm Release for Node.js App**
+resource "helm_release" "node_app" {
+  name       = "node-app"
+  repository = "https://abdullahtarar.github.io/node-app-helm/"
+  chart      = "node-app"
+  namespace  = "default"
+
+  set {
+    name  = "replicaCount"
+    value = 2
+  }
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "image.repository"
+    value = "abdullahtarar222/demo-node-app"
+  }
+
+  set {
+    name  = "image.tag"
+    value = "directory-app"
+  }
+
+  set {
+    name  = "autoscaling.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "node-app-sa"
+  }
+
+    set {
+    name  = "ingress.enabled"
+    value = "true"
+  }
+    set {
+    name  = "ingress.className"
+    value = "alb"
+  }
+
+
+  depends_on = [module.eks] # ✅ FIXED
+}
